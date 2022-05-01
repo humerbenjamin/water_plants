38474eaf196da842bd9482babfec56a65e4999fc
Benjamin Humer <bhumer@telus.net>
2022-05-01 12:52:38 -0400

    Add initilaization files
    initialize.cpp, initialize.hpp, settings.txt

---
+++ src/intitialize/initialize.hpp
@@ -0,0 +1,18 @@
+#ifndef INITIALIZE_HPP
+#define INITIALIZE_HPP
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+struct settings{
+    int numplants;
+    bool humidity;
+    bool temp;
+    bool vis;
+    bool indiv_water;
+};
+
+
+#endif
